40da22235ab489bf52c0f2498470ebe15a30257a
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 01:24:08 +0530

    Feat: integrate return logic with Book class and included fine info

--- Library Management System/return_book.py
+++ Library Management System/return_book.py
@@ -1,10 +1,27 @@
-from utils import books, issued_books
+from utils import books, Book
 
 def return_book():
     book_name = input("Enter the name of the book you want to return: ").strip().upper()
-    if book_name in issued_books:
-        issued_books.remove(book_name)
-        books.append(book_name)
-        print(f"You have returned '{book_name}'.")
+    author_name = input("Enter the name of the author: ").strip().upper()
+    student_name = input("Enter your name: ").strip().upper()
+    roll_number = input("Enter your roll number: ").strip().upper()
+
+    # Check if the student has issued the book
+    match = [i for i in books.values() if i.name == book_name and i.author == author_name and i.status == "issued" and i.issued_roll == roll_number and i.issued_by == student_name]
+    
+    if match:
+        match[0].status = "available"
+        match[0].issued_by = None
+        match[0].issued_roll = None
+        match[0].issue_date = None
+        match[0].return_date = None
+        match[0].finecalc()
+        print(f"{student_name} have returned '{match[0].name}' by {match[0].author} (ID: {match[0].id}) on {match[0].return_date.strftime('%d-%m-%Y')}.")
+        if match[0].fine != 0:
+            print(f"Fine: Rs. {match[0].fine}")
+            match[0].fine = 0
         return
-    print(f"Sorry, '{book_name}' is not available.")
+
+    print(f"Sorry, '{book_name}' by {author_name} is not issued to you.")
+    
+    
