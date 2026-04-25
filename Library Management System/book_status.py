13d72d1c4f491f7ba054bdba504913e8b4427dd9
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 02:50:06 +0530

    Feat: Implement book_status function (replace placeholder)

--- Library Management System/book_status.py
+++ Library Management System/book_status.py
@@ -1,2 +1,10 @@
+from utils import books
+
 def book_status():
+    # show list that which by which author got issued to whom and when
+    print("""\n====== BOOK STATUS ======\n""")
+    for i in books.values():
+        if i.status == "issued":
+            print(f"ID: {i.id} | Title: {i.name} | Author: {i.author} | Issued To: {i.issued_by} | Roll No: {i.issued_roll} | Issue Date: {i.issue_date} | Return Date: {i.return_date}")
+    print("""\n========================\n""")
     return
