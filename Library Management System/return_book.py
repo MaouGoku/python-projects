bd16d372d0c8de99cb59e4120e70c489264e07cd
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 02:54:59 +0530

    Feat: include return date in return message

--- Library Management System/return_book.py
+++ Library Management System/return_book.py
@@ -1,4 +1,5 @@
 from utils import books, Book
+import datetime as dt
 
 def return_book():
     book_name = input("Enter the name of the book you want to return: ").strip().upper()
@@ -10,16 +11,20 @@ def return_book():
     match = [i for i in books.values() if i.name == book_name and i.author == author_name and i.status == "issued" and i.issued_roll == roll_number and i.issued_by == student_name]
     
     if match:
+        return_date_now = dt.datetime.now().date()
+        match[0].finecalc()
+        fine_amount = match[0].fine
+        
         match[0].status = "available"
         match[0].issued_by = None
         match[0].issued_roll = None
         match[0].issue_date = None
         match[0].return_date = None
-        match[0].finecalc()
-        print(f"{student_name} have returned '{match[0].name}' by {match[0].author} (ID: {match[0].id}) on {match[0].return_date.strftime('%d-%m-%Y')}.")
-        if match[0].fine != 0:
-            print(f"Fine: Rs. {match[0].fine}")
-            match[0].fine = 0
+        match[0].fine = 0
+        
+        print(f"{student_name} have returned '{match[0].name}' by {match[0].author} (ID: {match[0].id}) on {return_date_now.strftime('%d-%m-%Y')}.")
+        if fine_amount != 0:
+            print(f"Fine: Rs. {fine_amount}")
         return
 
     print(f"Sorry, '{book_name}' by {author_name} is not issued to you.")
