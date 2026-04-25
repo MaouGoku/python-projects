918e143f43fc83ce67c40491afc8e1c5900d8bad
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 01:25:43 +0530

    Feat: add fine calculation method to Book class

--- Library Management System/utils.py
+++ Library Management System/utils.py
@@ -1,6 +1,6 @@
 # Main Data Storage
 import datetime as dt
-import random as rd
+import math
 
 books = {}
 class Book:
@@ -16,6 +16,19 @@ class Book:
         self.fine = 0
     def __str__(self):
         return f"ID: {self.id} | Title: {self.name} | Author: {self.author} | Status: {self.status}"
+
+    def finecalc(self):
+        if self.status == "issued":
+            if dt.datetime.now().date() > self.return_date:
+                late_days = (dt.datetime.now().date() - self.return_date).days
+                week = math.ceil(late_days / 7)
+                fine = 0
+                for i in range(1, week + 1):
+                    if i == week:
+                        fine += 10 * math.factorial(i) * (late_days - (week-1) * 7)
+                    else:
+                        fine += 10 * math.factorial(i) * 7
+                self.fine = fine
 
 
 
