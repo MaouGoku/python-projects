eff6630d20ff4749bdad2731ee9748bed874cf56
MaouGoku <rajdeepc362@gmail.com>
2026-04-25 23:10:42 +0530

    Introduced Book Class

--- Library Management System/utils.py
+++ Library Management System/utils.py
@@ -1,3 +1,23 @@
 # Main Data Storage
+import datetime as dt
+import random as rd
+
 books = {}
-book_inventory = {}
+class Book:
+    def __init__(self, id, name, author):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.status = "available"
+        self.issue_date = None
+        self.return_date = None
+        self.issued_by = None
+        self.issued_roll = None
+        self.fine = 0
+    def __str__(self):
+        return f"ID: {self.id} | Title: {self.name} | Author: {self.author} | Status: {self.status}"
+
+
+
+
+        
