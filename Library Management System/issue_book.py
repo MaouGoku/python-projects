0bb801a4056799a14b4d0e31951658ea9500be97
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 00:17:55 +0530

    Refactor: switch book issuing logic to Book classs

--- Library Management System/issue_book.py
+++ Library Management System/issue_book.py
@@ -1,12 +1,24 @@
-from utils import books, issued_books
+from utils import books, Book
+import datetime as dt
 
 def issue_book():
     book_name = input("Enter the name of the book you want to issue: ").strip().upper()
-    # for book in books:
-    # if book['name'] == book_name:
-    if book_name in books:
-        issued_books.append(book_name)
-        books.remove(book_name)
-        print(f"You have issued '{book_name}'.")
+    author_name = input("Enter the name of the author: ").strip().upper()
+    student_name = input("Enter your name: ").strip().upper()
+    roll_number = input("Enter your roll number: ").strip().upper()
+    issue_date =  dt.datetime.now().date()
+    
+    match = [i for i in books.values() if i.name == book_name and i.author == author_name and i.status == "available"]
+    
+    for i in match:
+        print(i)
+
+    if match:
+        match[0].status = "issued"
+        match[0].issued_by = student_name
+        match[0].issued_roll = roll_number
+        match[0].issue_date = issue_date
+        match[0].return_date = issue_date + dt.timedelta(days=7)
+        print(f"{student_name} have issued '{match[0].name}' (ID: {match[0].id}) on {match[0].issue_date.strftime("%d-%m-%Y")}. Due date: {match[0].return_date.strftime("%d-%m-%Y")}.")
         return
     print(f"Sorry, '{book_name}' is not available.")
