f6b1fdc52604b45cb5484f6a9771914037686f26
MaouGoku <rajdeepc362@gmail.com>
2026-04-26 19:42:49 +0530

    Refactor: redesign show_books output using structured table format

--- Library Management System/show_books.py
+++ Library Management System/show_books.py
@@ -1,16 +1,41 @@
-from utils import books, Book
+from utils import books
 
 def show_books():
+    # Checks if books are added or not 
     if not books:
-        print("\nNo books available in the library.\n")
+        print("\nNo books added in the library.\n")
         return
-        
-    print("\n====== BOOKS AVAILABLE IN THE LIBRARY ======\n")
-    print("| TITLE | AUTHOR | COUNT | ")
-    l=[]
-    for i in books.values():
-        if i.status == "available":
-            if (i.name,i.author) not in l:
-                print(f"| {i.name} | {i.author} | {i.count_book()} |")
-                l.append((i.name,i.author))
-    print("\n============================================\n")
+
+    # Fetches available books in library 
+    available = [b for b in books.values() if b.status == "available"]
+    
+    # if all books are issued 
+    if not available:
+        print("\nAll books are issued.\n")
+        return
+    
+    # Makes unique books
+    unique = {(b.name, b.author): b for b in available}
+    
+    # Variables for Menu Design  
+    padding = 4
+    title_width = max(len(b.name) for b in unique.values()) + padding
+    author_width = max(len(b.author) for b in unique.values()) + padding
+    count_width = max(len(str(b.count_book())) for b in unique.values()) + padding
+
+    header = f"║ {'TITLE':^{title_width}} ║ {'AUTHOR':^{author_width}} ║ {'COUNT':^{count_width}} ║"
+    top = "╔"+"═"*(title_width+2)+"╦"+"═"*(author_width+2)+"╦"+"═"*(count_width+2)+"╗"
+    middle = "╠"+"═"*(title_width+2)+"╬"+"═"*(author_width+2)+"╬"+"═"*(count_width+2)+"╣"
+    bottom = "╚"+"═"*(title_width+2)+"╩"+"═"*(author_width+2)+"╩"+"═"*(count_width+2)+"╝"
+    decorator = f"<<{' LIBRARY ':-^{len(top)-4}}>>"
+
+    # Menu Display Sequence
+    print("\nThe available books are listed below:\n")
+    print(decorator)
+    print(top)
+    print(header)
+    print(middle)
+
+    for i in unique.values():
+        print(f"║ {i.name : ^{title_width}} ║ {i.author : ^{author_width}} ║ {i.count_book() : ^{count_width}} ║")
+    print(bottom)
